c048d71bec198996c79fd48ea59db0c85784e8a5
D4B2A <128427718+D4B2A@users.noreply.github.com>
2023-03-25 18:52:41 +0100

    Update directRegister.cpp
    Moved namespace to directRegister.h

--- src/directRegister.cpp
+++ src/directRegister.cpp
@@ -1,21 +1,6 @@
 #include<"arduino.h">
 #include<"directRegister.h">
 
-namespace INT0{
-  void enableInterrupt();
-  void disableInterrupt();
-  void settupInterrupt(int);
-  bool readFlag();
-  void resetFlag();
-}
-
-namespace INT1{
-  void enableInterrupt();
-  void disableInterrupt();
-  void settupInterrupt(int state);
-  bool readFlag();
-  void resetFlag();
-}
 
 INT0::enableInterrupt(){
   EIMSK |= 1;
