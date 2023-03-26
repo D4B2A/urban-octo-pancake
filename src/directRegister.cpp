abb54a2c77771f1052c05d7556e94c97460057d6
D4B2A <128427718+D4B2A@users.noreply.github.com>
2023-03-26 12:24:21 +0200

    Update directRegister.cpp
    Added PCI

--- src/directRegister.cpp
+++ src/directRegister.cpp
@@ -42,3 +42,37 @@ INT1::readFlag(){
 INT1::resetFlag(){
   EIFR |= 2;
 }
+
+PCI::enableInterrupt(int interruptNumber){
+  PCICR |= 1<<interruptNumber;
+}
+
+PCI::disableInterrupt(int interruptNumber){
+  PCICR &= ~(1<<interruptNumber);
+}
+
+PCI::addPin(int pin){
+  switch(pin){
+    case 8..13:
+      PCMSK0 |= 1<<(pin-8);
+      break;
+    case 0..7:
+      PCMSK2 |= 1<<pin;
+      break;
+  }
+}
+
+PCI::removePin(int pin){
+  switch(pin){
+    case 0..7:
+      PCMSK2 &= ~(1<<pin);
+      break;
+    case 8..13:
+      PCMSK0 &= ~(1<<(pin-8));
+      break;
+  }
+}
+
+PCI::readFlag(int interruptNumber){
+  return PCIFR&(1<<interruptNumber);
+}
